c0167a929814b888d69a916dba1f716f915b6d5d
smbrown22 <smbrown22@my.waketech.edu>
2026-04-14 11:56:41 +0000

    5 tests

--- test_ticket_validator.py
+++ test_ticket_validator.py
@@ -3,6 +3,25 @@ from unittest.mock import Mock
 from ticket_validator import validate_ticket, get_ticket_tier, calculate_total 
 
 
-def test_validate_ticket_pass(code):
-    assert validate_ticket(code) is str and len(code) == 8 and code(0 , 1) is 'TX'
+def test_validate_ticket_pass():
+    ticket = "TX123456"
+    assert validate_ticket(ticket) is True 
 
+def test_invalid_validate_ticket_pass_numbers():
+    ticket = "TFA23456"
+    assert validate_ticket(ticket) is False
+
+def test_invalid_validate_ticket_pass_string_length():
+    ticket = "TX12345"
+    assert validate_ticket(ticket) is False
+
+def test_get_ticket_tier():
+    ticket = "TX123456"
+    assert get_ticket_tier(ticket) == "General"
+
+def test_get_ticket_tier_invalid():
+    ticket = "TX12345"
+    assert get_ticket_tier(ticket) raise ValueError
+
+
+    
